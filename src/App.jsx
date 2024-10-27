21446d12cd6693f95b2fa228e76675ad14345a5d
Avery Jacobson <averyajacobson@gmail.com>
2024-10-27 14:09:17 -0500

    added footer and content and made my links pretty

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import React from 'react';
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
-
-
-
-// function App() {
-//   return (
-//     <div id="root">
-//       <Header />
-
-//     </div>
-//   );
-// }
+import '../styles.css';
 
 
 function App() {
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,23 @@
 import React, { useState } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-
+import '../../footerStyles.css';
 
 const Footer = () => {
-
   return (
-    <>
-      <Navbar bg="dark" variant="dark" expand="lg" className="w-100" style={{ width: '100%' }}>
-        <Container fluid className="d-flex justify-content-between align-items-center">
-          {/* Left-aligned brand name */}
-          <Navbar.Brand href="#home" style={{ color: 'white', textDecoration: 'none' }}>
-            This is my footer
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-
-    </>
+    <Navbar bg="dark" variant="dark" expand="lg" className="w-100" style={{ width: '100%' }}>
+      <section className="links">
+        <div className="linkedin">
+          <a href="https://www.linkedin.com/in/avery-jacobson-aa36225a/" target="_blank" rel="noopener noreferrer" className="link-button">
+            LinkedIn
+          </a>
+        </div>
+        <div className="github">
+          <a href="https://github.com/TheReal4m4d3u5" target="_blank" rel="noopener noreferrer" className="link-button">
+            GitHub
+          </a>
+        </div>
+      </section>
+    </Navbar>
   );
 };
 
