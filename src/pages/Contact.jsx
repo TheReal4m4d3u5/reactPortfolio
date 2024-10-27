2d75c69f827bb61e227565a0cc1ec6ddb7b1fc17
Avery Jacobson <averyajacobson@gmail.com>
2024-10-27 13:53:01 -0500

    added error messages to inputfiels in contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import '../../contactSytles.css';
 
 
 function Contact() {
+
+  const [errors, setErrors] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleBlur = (field, value) => {
+    if (!value) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        [field]: 'This field is required',
+      }));
+    } else {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        [field]: '',
+      }));
+    }
+  };
+
+
+
   return (
     <div className="contact-page">
       <div className="contact-heading">
@@ -12,15 +35,36 @@ function Contact() {
       <form>
         <div className="mb-3">
           <label htmlFor="name" className="form-label">Name</label>
-          <input type="text" className="form-control" id="name" placeholder="Enter your name" />
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            placeholder="Enter your name"
+            onBlur={(e) => handleBlur('name', e.target.value)}
+          />
+          {errors.name && <p className="error-message">{errors.name}</p>}
         </div>
         <div className="mb-3">
           <label htmlFor="email" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="email" placeholder="name@example.com" />
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            placeholder="name@example.com"
+            onBlur={(e) => handleBlur('email', e.target.value)}
+          />
+          {errors.email && <p className="error-message">{errors.email}</p>}
         </div>
         <div className="mb-3">
           <label htmlFor="message" className="form-label">Message</label>
-          <textarea className="form-control" id="message" rows="4" placeholder="Enter your message"></textarea>
+          <textarea
+            className="form-control"
+            id="message"
+            rows="4"
+            placeholder="Enter your message"
+            onBlur={(e) => handleBlur('message', e.target.value)}
+          ></textarea>
+          {errors.message && <p className="error-message">{errors.message}</p>}
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
