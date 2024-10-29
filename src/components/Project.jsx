f95d561018a68aafd7d7f9e49433b15a4804517d
Avery Jacobson <averyajacobson@gmail.com>
2024-10-28 22:45:25 -0500

    fixed link not going to git

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,27 +5,25 @@ import '../styles/projectStyles.css';
 
 const Project = ({ title, description, image, link }) => {
   return (
-    
     <Card className="project-card h-100">
       <Card.Img variant="top" src={image} alt={`${title} screenshot`} className="project-image" />
       <div className="myCard">
-      <Card.Body className="d-flex flex-column">
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>{description}</Card.Text>
-        <a
-  role="button"
-  tabindex="0"
-  href=""
-  target="_blank"
-  rel="noopener noreferrer"
-  className="mt-auto btn btn-primary custom-btn"
->
-  View Project
-</a>
-      </Card.Body>
+        <Card.Body className="d-flex flex-column">
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{description}</Card.Text>
+          <a
+            role="button"
+            tabIndex="0"
+            href={link}  // Use the link prop here
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mt-auto btn btn-primary custom-btn"
+          >
+            View Project
+          </a>
+        </Card.Body>
       </div>
     </Card>
-    
   );
 };
 
