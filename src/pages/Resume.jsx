4435b7bb089641d3bc072c4708163f02271ad719
Avery Jacobson <averyajacobson@gmail.com>
2024-11-15 15:02:47 -0600

    added resume css, skills and jobs

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -19,9 +19,11 @@ const Resume = () => {
         </div>
 
 
+
         <section className="professional-section">
 
           <h3 className='resumeHeaders'>Professional Summary</h3>
+          <h3 className='coreSkills'>Core Skills</h3>
           <section className='professinalText'>
             <p>Full Stack developer with 10 years as Senior Software Development Engineer in Test (SDET).</p>
 
@@ -30,13 +32,19 @@ const Resume = () => {
 
         <div className="skills-container">
           <div className="skill-item">Programming Languages</div>
-          <div className="skill-item">Tools & Technologies</div>
-          <div className="skill-item">Hardware</div>
         </div>
 
+        <div>C, C++, C#, Java, Python, JavaScript, Visual Basic .NET, Bash, Batch, SQl, LISP, Assembly language, OpenGL(Libraries), GLUT, Equalizer, Glee, GLEW, GLUS, GLM, HTML, CSS, PHP, Perl, AngularJS, XML, AJAX, Jquery, VB script, VBA</div>
 
 
-        <h3 className='coreSkills'>Core Skills</h3>
+        <div className="skill-item">Tools & Technologies</div>
+        <div>•	Linux, Unix, GitHub, RTOS, GCC, Visual Studio, Jira, IAR, Matlab, Labview, Xilinx, Eclipse, DrRacket, Python 2.7x, EASy68k, Apache Subversion, Hercules, Cygwin, MinGW, Doors, IBM Notes, Microsoft Office, Testlink, Test Rail, Test Complete, Redmine, Apache, Harvest, ADE, Avionics RTOS, ARM Cortex, AVR ATMega, GSM, CDMA, accelerometer, GPS, Ublocks, ZigBee, Infrared, Arduino, Basys2, Raspberry Pi, ICs, JTAG, UART, oscilloscope, frequency generator, spectrum analyzer, digital multi meter, D/C power supply, Arinc 664, Arinc 429, RS-232, network switch.</div>
+        <div className="skill-item">Hardware</div>
+        <div>•	A/C D/C, accelerometer, ADE (bus management software), Apache, Arduino, Arinc protocols, ARM Cortex, AVR (microcontroller), Basys2, CDMA, controller software, Cygwin, DLL, digital logic, Doors, digital multi meter, Eclipse, DHCP, frequency generator, FPGA, GCC, GIT, GitHub, GPS, GSM, Harvest, Hercules, IAR Systems, IBM Notes, infrared, integrated circuits, Jira, JTAG, Labview, Linux, Matlab, MS Office, MS Teams, MinGW, network switch, oscilloscope, Raspberry Pi, real time operating systems, Redmine, RS-232, spectrum analyzer, Subversion, TCP/IP, TestComplete, Testlink, Test Rail, UART, Ublocks, Unix, Virtual Box, Visual Studio, Xilinx, ZigBee </div>
+
+
+
+
 
 
         <section className="education-ection">
@@ -55,6 +63,78 @@ const Resume = () => {
           <h3 className='resumeHeaders'>Professoinal Experience</h3>
           <ul>
             <li>Worked the last four years as a senior software Engineer for Wells Fargo as a sdet</li>
+            <div></div>
+            <li>Wells Fargo Eden Prairie, MN  							  3/20 – Current
+              Application System Engineer
+              •	a team to select a version control tool and research plug-ins needed to work with our automation tool.
+              •	Designed workflow of software automation development and set up the workflow tool Jira.
+              •	Working in Core, Well’s Fargo’s Home Lending tool. Usuring that there are not defect, all products are returning and that the tool is working correctly in the Sales Consultation space.
+              •	Scrum, Agile team
+              •	Functional UI testing, creating unit tests for front UI development work.
+              •	Develop Java Selenium code for automation.
+              •	Create testing documentation in the APplciation Lifecycle Management tool.
+              •	Work with the Rules and GAIP team to make sure products are returning correctly.
+              •	Work on End to End testing teams to seed loans to test from start to finish.
+            </li>
+            <div></div>
+
+            <li>Stratasys Eden Prairie, MN 							  9/17 – 12/19
+              Senior Software Technician
+              •	Helped consult and set up a test case management tool TestRail. Led the drive to work with a team to select a version control tool and research plug-ins needed to work with our automation tool.
+              •	Designed workflow of software automation development and set up the workflow tool Jira.
+              •	Set up Jira using simple issue language and REST API.
+              •	Consulted on software languages being used for automation development.
+              •	Wrote new and updated old test cases for a variety of industrial 3D printers.
+              •	Worked with engineers on upcoming projects on tools needed for systems, and to diagnose failures and change requests on current projects.
+              •	Set up new automation suites and architecture for new projects, taking written test cases and automating them using the scripting tool TestComplete and writing test scripts using Javascript.
+              •	Writing automated tools using Batch, Bash and Python scripts to execute backend commands along with TestComplete.
+              •	Set up automation hardware to be ran remotely.
+              •	Created a virtual machine for automation to run on. Used a programmer to flash and test firmware.
+              •	Use RS-232 to look at serial data from the processor.
+              •	Execute manual software tests for specific change request, analyzing system logs to diagnose failures. Train summer interns and be their main point of contact.
+              •	Train in new employees to the team.
+              •	Created dashboards in Jira for data mining for all automation projects to report progress.
+              •	Won a spark award by the company for going above and beyond the call of my job.
+              Created Summary:  3D printers, Automated/Manuel, Bash, Batch, Cygwin, Embedded Systems, Linux, JavaScript, Jira, Python, Scripting, Spark Award, TestComplete, TestRail, Unit/Regression testing, Virtual Machine, WSL.
+            </li>
+
+            <li>Rockwell Collins Cedar Rapids, IA  							       1/17 – 8/17
+              Software Technician
+              •	Helped design, test, and develop multiple proprietary critical flight systems for aircraft industry.
+              •	Worked in an agile team environment holding scrum meetings allocating and executing tasks. Tasks included:
+              •	Assist systems engineer in development of buses on proprietary bus management software.
+              •	Helped add features to proprietary aviation software tools in PHP.
+              •	Set up automation scripts using python and fixing broken automation scripts.
+              •	Set up Bamboo in Atlassian to launch batch scripts I created to automate the execution of automation and logs.
+              •	Set up aviation hardware and software for testing on flight critical systems.
+            </li>
+
+
+            <div>Remote Technologies Inc Shakopee, MN  					3/16 – 9/16
+              Lead Test Engineer
+              •	Created and executed Automated and  tests on home automation hardware and software that was integrated with OEM, third party hardware and software.
+              •	Set up a test case management server that integrated  with the bug reporting tool Redmine.
+              •	Set up different A/C Z-Wave light switches and create test cases, testing the automaton software that accompanied the hardware.
+              •	Test remotes, control processors, wired in wall and mounted control touch display interfaces, Z-Wave, audio and video distribution matrices.
+              •	Set up professional home theater, audio systems and streaming interfaces (Apple TV, Google TV, Amazon TV, 4K, etc.), understanding the protocols and interfaces.
+              •	Created scripts from proprietary home automation software to test systems.
+            </div>
+
+            <li>Carestream Oakdale, MN 						05/18/15 – 11/19/15
+              Software Quality Assurance Technician
+              •	Tested, maintained and serviced different radiology printers for quality control with new hardware and software.
+              •	Follow FDA documentation regulations. Creating and maintained servers for printer interface. Followed test procedures and did verification testing with production image quality control.
+              •	Made sure that everything was shipped correctly and worked out of the box according to manuals. Tested prototype film for quality control and meeting of standards, including measuring film for correct density of darkness and followed image quality test procedures for quality inspection of film.
+              •	If an engineer made new hardware, I would put it in the machine and test that the X-ray imager was working properly, and that film met quality specifications.
+            </li>
+
+            <li>Xirgo, Technologies Inc Edina, MN                                                                           09/18/14 – 3/30/15
+              Software Test Engineer
+              •	Using the C programming language and Windows’ libraries, I created a test script to perform regression tests on positive and negative test cases; created a serial connection using the window's libraries.
+              •	A file handle was created, the baud rate was set, and the data was parsed.
+              •	Given a user manual and created test cases based on the device.
+              •	A time stamp was created at the beginning and end of every test case, results went to a HTML document.
+            </li>
           </ul>
         </section>
 
