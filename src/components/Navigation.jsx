ce4adeb414fe994246f528d334a64b79c0121716
Avery Jacobson <averyajacobson@gmail.com>
2024-11-18 18:23:02 -0600

    made about default

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,10 +5,13 @@ import '../styles/navigationStyles.css';
 const Navigation = ({ isOpen }) => {
   const location = useLocation();
   const currentPage = location.pathname;
-  console.log('Menu asdf asdf adstoggled:', !isOpen); 
+ 
+
+  const isDefaultPage = currentPage === '/about' || currentPage === '/';
+
   return (
     <nav className={`navigation ${isOpen ? 'open' : ''}`}>
-      <Link to="/about" className={currentPage === '/about' ? 'active' : ''}>About</Link>
+      <Link to="/about" className={isDefaultPage? 'active' : ''}>About</Link>
       <Link to="/contact" className={currentPage === '/contact' ? 'active' : ''}>Contact</Link>
       <Link to="/portfolio" className={currentPage === '/portfolio' ? 'active' : ''}>Portfolio</Link>
       <Link to="/resume" className={currentPage === '/resume' ? 'active' : ''}>Resume</Link>
