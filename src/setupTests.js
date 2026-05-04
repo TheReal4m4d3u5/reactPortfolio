01671f4cff93127f2459432694e42142a316831c
TheReal4m4d3u5 <averyajacobson@gmail.com>
2026-05-04 08:18:25 -0500

    made cards bigger

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,9 @@
 import "@testing-library/jest-dom";
+
+class MockIntersectionObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+global.IntersectionObserver = MockIntersectionObserver;
