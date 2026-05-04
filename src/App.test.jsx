9fb493f46ef41ad19e052e1d45625bd07cb53404
TheReal4m4d3u5 <averyajacobson@gmail.com>
2026-05-03 23:43:34 -0500

    added globals to viteconfig

---
+++ src/App.test.jsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import { createMemoryRouter, RouterProvider } from "react-router-dom";
+
+import App from "./App";
+import About from "./pages/About";
+
+test("renders the app without crashing", () => {
+  const router = createMemoryRouter([
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <About />,
+        },
+      ],
+    },
+  ]);
+
+  render(<RouterProvider router={router} />);
+
+  expect(screen.getByText(/about/i)).toBeInTheDocument();
+});
