d23d0170a4c0d46921729ea282edb19b2db6393e
TheReal4m4d3u5 <averyajacobson@gmail.com>
2026-05-03 23:58:28 -0500

    create test for all pages to load

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,6 +4,9 @@ import { createMemoryRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
 import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Resume from "./pages/Resume";
 
 test("renders the app without crashing", () => {
   const router = createMemoryRouter([
